7a0ac0fb0239ade4351536bce18b77415c3c2983
magnoliaceiling <alasdair@lomas.uk>
2020-05-07 05:27:57 +0100

    Font changed to 18, draw position moved to (0, 0)

--- script.py
+++ script.py
@@ -21,7 +21,7 @@ inky_display = InkyPHAT("red")
 inky_display.set_border(inky_display.WHITE)
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
-font = ImageFont.truetype(FredokaOne, 22)
+font = ImageFont.truetype(FredokaOne, 18)
 w, h = font.getsize(message)
 x = (inky_display.WIDTH / 2) - (w / 2)
 y = (inky_display.HEIGHT / 2) - (h / 2)
@@ -48,6 +48,6 @@ def reflow_text(quote, width, font):
 
     return reflowed
 reflowed_message = reflow_text(message, inky_display.WIDTH, font)
-draw.text((0, 20), reflowed_message, inky_display.BLACK, font)
+draw.text((0, 0), reflowed_message, inky_display.BLACK, font)
 inky_display.set_image(img)
 inky_display.show()
