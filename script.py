8c956cb65d6adac4e87414506e948de5a55e8588
magnoliaceiling <alasdair@lomas.uk>
2020-05-11 20:17:39 +0100

    Changed to functions

--- script.py
+++ script.py
@@ -4,17 +4,39 @@ from inky import InkyPHAT
 from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
 # Get the current message on the annunciator
-response = requests.get('https://dm-devci-annunciator-services.azurewebsites.net/api/Message/message/0/current')
+baseURLStart = 'https://dm-devci-annunciator-services.azurewebsites.net/api/Message/message/'
+baseURLEnding = '/current'
+house = 0
+response = requests.get(baseURLStart + str(house) + baseURLEnding)
 data = response.json()
-currentCommons = data['slides'][0]['lines']
-totalMess = len(currentCommons)
-messagePayload = totalMess - 1
-i = 0
-holder = []
-while messagePayload > i:
-    holder.append(data['slides'][0]['lines'][i]['content'])
-    i += 1
-message = ' '.join(holder).title()
+
+def withMember():
+    x = data['slides'][0]['lines']
+    y = len(x)
+    z = y - 1
+    i = 0
+    holder = []
+    while z > i:
+        holder.append(data['slides'][0]['lines'][i]['content'])
+        i += 1
+    string = ' '.join(holder).title()
+    return string
+
+def noMember():
+    x = data['slides'][0]['lines']
+    y = len(x)
+    i = 0
+    holder = []
+    while y > i:
+        holder.append(data['slides'][0]['lines'][i]['content'])
+        i += 1
+    string = ' '.join(holder).title()
+    return string
+
+if data['slides'][0]['lines'][-1]['member'] is None:
+    message = noMember()
+else:
+    message = withMember()
 
 # Inky section shamelessly stolen from their guide https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-inky-phat
 inky_display = InkyPHAT("red")
